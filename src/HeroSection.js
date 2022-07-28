e128f9c6d8d66a1d24a8b4889e08bfbbd7267fa5
sagarpanwar1 <52241021+sagarpanwar1@users.noreply.github.com>
2022-07-28 20:33:37 +0530

    init changes

--- src/HeroSection.js
+++ src/HeroSection.js
@@ -9,7 +9,7 @@ const HeroSection = () => {
           necessitatibus, quas voluptas beatae optio maxime cupiditate at fuga
           officiis odit rem provident nobis esse magni! Lorem ipsum dolor sit amet
           consectetur, adipisicing elit. Assumenda velit dolorem repudiandae qui
-          rerum possimus sit animi. Illo, laudantium ea!
+          rerum possimus sit animi. Illo, laudantium ea!kjdkjqhdkjqd
         </p>
         <hr className="my-4" />
         <p>
