afe2af1d7c2a607d83fca612d8e9425b8899f090
sagarpanwar1 <52241021+sagarpanwar1@users.noreply.github.com>
2022-07-28 20:47:16 +0530

    revert changes back to original

--- src/HeroSection.js
+++ src/HeroSection.js
@@ -9,7 +9,7 @@ const HeroSection = () => {
           necessitatibus, quas voluptas beatae optio maxime cupiditate at fuga
           officiis odit rem provident nobis esse magni! Lorem ipsum dolor sit amet
           consectetur, adipisicing elit. Assumenda velit dolorem repudiandae qui
-          rerum possimus sit animi. Illo, laudantium ea!kjdkjqhdkjqd
+          rerum possimus sit animi. Illo, laudantium
         </p>
         <hr className="my-4" />
         <p>
@@ -24,5 +24,6 @@ const HeroSection = () => {
 }
 
 export default HeroSection;
+
 
 
